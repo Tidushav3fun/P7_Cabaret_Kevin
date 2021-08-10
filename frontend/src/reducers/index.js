1562cca6f4d37417bc5cb6aa0cc3ebacb5ac99a9
Tidushav3fun <69478381+Tidushav3fun@users.noreply.github.com>
2021-08-10 22:09:33 +0200

    Delete frontend/src directory

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import authReducer from './authReducer'
-
-import messageReducer from './messageReducer'
-
-export default combineReducers({
-  authReducer,
-  messageReducer
-})
